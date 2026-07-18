e050e4f7faef1698381e8004d12319578869c7d8
Burcu Süzer <burcusuzer@hotmail.com>
2026-07-18 16:01:01 +0300

    Update customer lifetime value abalysis

--- queries/07_customer_lifetime_value.sql
+++ queries/07_customer_lifetime_value.sql
@@ -3,101 +3,111 @@ Project: DVD Rental Business Performance Analysis
 Author: Burcu Süzer
 
 Business Question:
-Which customers have the highest lifetime value?
+Who are the top 10 customers by lifetime value (CLV)?
 */
 
+
+
 WITH customer_metrics AS (
-    SELECT
+    SELECT  
         c.customer_id,
-        c.first_name,
-        c.last_name,
-        SUM(p.amount) AS lifetime_value
-    FROM
+        CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
+        COUNT(r.rental_id) AS total_rentals,
+        SUM(p.amount) AS lifetime_value,
+        MIN(r.rental_date) AS first_rental,
+        MAX(r.rental_date) AS last_rental
+    FROM 
         customer c
-        JOIN payment p ON c.customer_id = p.customer_id
-    GROUP BY
-        c.customer_id,
-        c.first_name,
-        c.last_name
-)
-SELECT
-    ranking,
-    customer_id,
-    first_name,
-    last_name,
-    lifetime_value
-FROM
-    (
-        SELECT
-            ROW_NUMBER() OVER (
-                ORDER BY
-                    lifetime_value DESC
-            ) AS ranking,
-            customer_id,
-            first_name,
-            last_name,
-            lifetime_value
-        FROM
-            customer_metrics
-    ) ranked
-WHERE
-    ranking <= 10
-ORDER BY
-    ranking;
-/*
- Project: DVD Rental Business Performance Analysis
- Author: Burcu Süzer
- 
- Bonus Business Question:
- What percentage of the total revenue does each customer contribute?
- */
-WITH customer_metrics AS (
-    SELECT
-        c.customer_id,
-        c.first_name,
-        c.last_name,
-        SUM(p.amount) AS lifetime_value
-    FROM
-        customer c
-        JOIN payment p ON c.customer_id = p.customer_id
+    JOIN rental r 
+        ON
+        c.customer_id = r.customer_id
+    JOIN payment p
+        ON
+        p.rental_id = r.rental_id
     GROUP BY
         c.customer_id,
         c.first_name,
         c.last_name
 ),
-overall_metrics AS (
-    SELECT
-        SUM(lifetime_value) AS total_lifetime_value
-    FROM
+
+ranked_customers AS (
+    SELECT 
+        customer_id,
+        customer_name,
+        total_rentals,
+        lifetime_value,
+        first_rental,
+        last_rental,
+        RANK() OVER(ORDER BY lifetime_value DESC) AS customer_rank
+    FROM 
         customer_metrics
 )
+
 SELECT
-    ranking,
+    customer_rank,
     customer_id,
-    first_name,
-    last_name,
+    customer_name,
+    total_rentals,
     lifetime_value,
-    lifetime_value_pct
+    first_rental,
+    last_rental
 FROM
-    (
-        SELECT
-            ROW_NUMBER() OVER (
-                ORDER BY
-                    c.lifetime_value DESC
-            ) AS ranking,
-            c.customer_id,
-            c.first_name,
-            c.last_name,
-            c.lifetime_value AS lifetime_value,
-            ROUND(
-                c.lifetime_value * 100 / o.total_lifetime_value,
-                2
-            ) AS lifetime_value_pct
-        FROM
-            customer_metrics c
-            CROSS JOIN overall_metrics o
-    ) ranked
+    ranked_customers
 WHERE
-    ranking <= 10
+    customer_rank <= 10
 ORDER BY
-    ranking;
+    customer_rank;
+
+/*
+ Project: DVD Rental Business Performance Analysis
+ Author: Burcu Süzer
+ 
+ Bonus Business Question:
+ Which customers have a lifetime value above the average customer lifetime value?
+ */
+
+
+WITH customer_metrics AS (
+    SELECT  
+        c.customer_id,
+        CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
+        COUNT(r.rental_id) AS total_rentals,
+        SUM(p.amount) AS lifetime_value,
+        MIN(r.rental_date) AS first_rental,
+        MAX(r.rental_date) AS last_rental
+    FROM 
+        customer c
+    JOIN rental r 
+        ON
+        c.customer_id = r.customer_id
+    JOIN payment p
+        ON
+        p.rental_id = r.rental_id
+    GROUP BY
+        c.customer_id,
+        c.first_name,
+        c.last_name
+),
+
+average_lifetime_value AS (
+    SELECT 
+        AVG(lifetime_value) AS avg_clv
+    FROM 
+        customer_metrics
+)
+
+SELECT 
+    cm.customer_id,
+    cm.customer_name,
+    cm.total_rentals,
+    cm.lifetime_value,
+    cm.first_rental,
+    cm.last_rental
+FROM 
+    customer_metrics cm
+CROSS JOIN 
+    average_lifetime_value alv
+WHERE
+    cm.lifetime_value > alv.avg_clv
+ORDER BY
+    cm.lifetime_value DESC;
